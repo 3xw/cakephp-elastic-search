d4700f71a423f1ba63293c546dc8ea5e7d10288b
Wallef Antoine <antoine.wallef@gmail.com>
2018-03-06 09:18:58 +0100

    first drop now test

--- src/Model/Behavior/SyncWithESBehavior.php
+++ src/Model/Behavior/SyncWithESBehavior.php
@@ -25,6 +25,25 @@ class SyncWithESBehavior extends Behavior
 
   protected $_items = [];
 
+  public function beforeSave(Event $event, EntityInterface $entity, \ArrayObject $options)
+  {
+    if($this->_config['translate']) foreach(Configure::read('I18n.languages') as $locale) $this->items[] = $this->setEsEntity($entity, $locale);
+    else $this->items[] = $this->setEsEntity($entity);
+  }
+
+  public function afterSave(Event $event, EntityInterface $entity, \ArrayObject $options)
+  {
+    foreach($this->_items as $item) $this->getType()->save($this->getType()->patchEntity($item, ['foreign_key' => $entity[$this->_config['mapping']['foreign_key']]]));
+  }
+
+  public function afterDelete(Event $event, EntityInterface $entity, \ArrayObject $options)
+  {
+    $where = ['foreign_key' => $entity[$this->_config['mapping']['foreign_key']],'model' => $this->_table->getAlias()];
+    if($this->_config['translate']) $where['locale'] => $locale;
+    $item = $this->getType()->find()->where($where)->first();
+    $this->getType()->delete($item);
+  }
+
   public function getType()
   {
     if($this->Type == null) $this->Type = TypeRegistry::get($this->_config['type']);
@@ -56,16 +75,5 @@ class SyncWithESBehavior extends Behavior
     $where = ['foreign_key' => $entity[$this->_config['mapping']['foreign_key']],'model' => $this->_table->getAlias()];
     if($this->_config['translate']) $where['locale'] => $locale;
     return $this->getType()->patchEntity($this->getType()->find()->where($where)->first(), $this->retriveData($entity, $locale));
-  }
-
-  public function beforeSave(Event $event, EntityInterface $entity, \ArrayObject $options)
-  {
-    if($this->_config['translate']) foreach(Configure::read('I18n.languages') as $locale) $this->items[] = $this->setEsEntity($entity, $locale);
-    else $this->items[] = $this->setEsEntity($entity);
-  }
-
-  public function afterSave(Event $event, EntityInterface $entity, \ArrayObject $options)
-  {
-    foreach($this->_items as $item) $this->getType()->save($this->getType()->patchEntity($item, ['foreign_key' => $entity[$this->_config['mapping']['foreign_key']]]));
   }
 }
