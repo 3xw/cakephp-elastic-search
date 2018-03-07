925302aedf67b5c4b6ab54739939409cd8a798cf
Wallef Antoine <antoine.wallef@gmail.com>
2018-03-07 16:53:37 +0100

    import

--- src/Shell/Task/ImportTask.php
+++ src/Shell/Task/ImportTask.php
@@ -2,14 +2,23 @@
 namespace Trois\ElasticSearch\Shell\Task;
 
 use Cake\Utility\Inflector;
-use Cake\Filesystem\File;
+use Cake\ElasticSearch\TypeRegistry;
+use Cake\ORM\TableRegistry;
 use Cake\Network\Http\Client;
 
 class importTask extends ElasticeSearchConnectTask
 {
   public $client = null;
 
-  public function main($index = null, $model = null)
+  public $index = null;
+
+  public $type = null;
+
+  public $properties = null;
+
+  public $table = null;
+
+  public function main($index = null, $table = null)
   {
     // set index
     if($index == null)
@@ -19,10 +28,10 @@ class importTask extends ElasticeSearchConnectTask
     }
     $index = Inflector::slug($index,'_');
 
-    $this->testIndex($index, $model);
+    $this->testIndex($index, $table);
   }
 
-  public function testIndex($index, $model)
+  public function testIndex($index, $table)
   {
     $url = ($this->connection->config()['port'] == 443)? 'https://': 'http://';
     $url .= $this->connection->config()['host'].'/'.$index;
@@ -31,17 +40,98 @@ class importTask extends ElasticeSearchConnectTask
     $response = $this->client->get($url);
     if($response->code == 200)
     {
-      $this->collectMapping(json_decode($response->body(),true),$index,$model);
+      $this->index = $index;
+      $this->collectMapping(json_decode($response->body(),true),$table);
     }else
     {
       $this->err('error with index:"'.$index.'"');
       debug(json_decode($response->body(),true));
-      $this->main($index, $model);
+      return $this->main($index, $table);
     }
   }
 
-  public function collectMapping($json, $index, $model)
+  public function collectMapping($json, $table)
   {
-    debug($json);
+    if(empty($json[$this->index]['mappings'])){
+      $this->err('no mappings found for index:"'.$index.'"');
+      return $this->main(null, $table);
+    }
+
+    // collect types
+    $types = array_keys($json[$this->index]['mappings']);
+    $type = $this->in('In which table/type would you import MySQL data?',$types,$types[0]);
+    $this->properties = $json[$this->index]['mappings'][$type]['properties'];
+
+    // test model now
+    $this->type = TypeRegistry::get($type);
+    $this->testTable($table);
   }
+
+  public function testTable($table)
+  {
+    if($table == null) $table = $this->in('Which table you like to import ?');
+    $this->table = TableRegistry::get($table);
+    try {
+      $row = $this->table->find()->first();
+    } catch (\Exception $e) {
+      $this->err($e->getMessage());
+      return $this->testTable(null);
+    }
+
+    $this->createMapping();
+  }
+
+  public function createMapping()
+  {
+    // reset!
+    foreach($this->properties as $key => &$value) $value = null;
+
+    $this->info('Ok let\'s build the mapping now...');
+    foreach($this->properties as $key => &$value)
+    {
+      $value = null;
+      if($key == 'model') continue;
+      if($key == 'locale') continue;
+      if($this->in('assign ES "'.$key.'" key with a table\'s field?',['Y', 'N'],'Y') == 'N')continue;
+
+      foreach($this->table->getSchema()->columns() as $number => $field) $this->out($number.': '.$field);
+      $value = $this->in('Build ES "'.$key.'" with comma separated preceding fields:');
+    }
+
+    $count = 0;
+    foreach($this->properties as $key => &$value)
+    {
+      if($key == 'model') continue;
+      if($key == 'locale') continue;
+      if($value == null) continue;
+
+      $fields = explode(',', $value);
+      $count = (count($fields) > $count)? count($fields):$count;
+      $value = [];
+
+      foreach($fields as $field) $value[] = $this->table->getSchema()->columns()[trim($field)];
+    }
+
+    $this->info('Mapping for '.$this->type->alias().': ');
+    $mapping = [];
+    $mapping[] = array_keys($this->properties);
+    for($i = 0; $i < $count; $i++)
+    {
+      $row = [];
+      foreach($this->properties as $field => $f) $row[] = ($f == null || empty($f[$i]))? null: $f[$i];
+      $mapping[] = $row;
+    }
+    $this->helper('Table')->output($mapping);
+
+    if($this->in('looks good?',['Y', 'N'],'Y') == 'N') return $this->createMapping();
+
+    $this->import();
+  }
+
+  public function import()
+  {
+      //return debug($this->table->hasBehavior('Translate'));
+  }
+
+
 }
